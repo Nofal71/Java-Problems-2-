bf75d2906757212ad3445fed4f16688804a385ce
Nofal71 <hafiznofal76@gmail.com>
2023-11-28 02:23:54 -0800

    Oop

--- Array.java
+++ Array.java
@@ -1,115 +1,167 @@
 import java.util.Scanner;
 
-
-
-
-
-
-
 public class Array {
-    
-    public static class Large {
-        
-       
-      
-        Scanner Scc = new Scanner(System.in);   //Input Array
-        Scanner size = new Scanner(System.in);  //Size of Array
-        private int s;
-        private int[] array;   
+    Scanner scanner = new Scanner(System.in);
+
+    class Large {
         private int largest;
         private int smallest;
-        
-        void input(){
-        System.out.print("Enter Size of Array = ");
-        int s = size.nextInt();
-        array = new int[s];
-        System.out.print("Enter Elements of Array = ");
-        
-        for(int i = 0 ; i < s;i++){
-            array[i]= Scc.nextInt();
-        }
-        }
 
         void find() {
-            largest=array[0];
-            smallest=array[0];
+            System.out.print("Enter Size of Array = ");
+            int size = scanner.nextInt();
+            int array[] = new int[size];
+
+            System.out.print("Enter Elements of Array = ");
+            for (int i = 0; i < size; i++) {
+                array[i] = scanner.nextInt();
+            }
+
+            largest = array[0];
+            smallest = array[0];
+
             for (int i = 1; i < array.length; i++) {
                 if (largest < array[i]) {
                     largest = array[i];
-                }
-                else if (smallest > array[i]){
+                } else if (smallest > array[i]) {
                     smallest = array[i];
-                } 
+                }
             }
-            
         }
-        void Peak(){
-            System.out.println("Peak Element is = "+ largest);
-        }
-        void minimum(){
-            System.out.println("Minimum Element is = "+ smallest);
-        }
-        void maximum(){
-            System.out.println("Maximum Element is = "+ largest);
-        }
-     
-    
-    };
 
- public static class ReverseArray {
- 
-    Scanner Arr = new Scanner(System.in);
-    Scanner s = new Scanner(System.in);
-    
-   private int[] array;
-   private int size ;
-   private int[] array1;
-    
-    void Input(){
-        System.out.print("Enter Size of Array = ");
-        size = s.nextInt();
-        array = new int[size];
-        System.out.print("Enter Elements of Array = ");
-        for(int i = 0 ; i < size ;i++){
-        array[i] = Arr.nextInt();
+        void peak() {
+            System.out.println("Peak Element is = " + largest);
         }
-    }
-    void reverse(){
-        array1 = new int[size];
-        int i = 0 , j = size-1;
-         while (i < array.length) {
-            array1[i] = array[j];
-            i++;
-            j--;
-        }
-        for (int k = 0; k < array1.length; k++) {
-            System.out.print(array1[k] + " ");
-        
 
+        void minimum() {
+            System.out.println("Minimum Element is = " + smallest);
         }
-    
-        
+
+        void maximum() {
+            System.out.println("Maximum Element is = " + largest);
         }
     }
 
+    class ReverseArray {
+        private int[] array;
+        private int size;
+        private int[] reversedArray;
+
+        void input() {
+            System.out.print("Enter Size of Array = ");
+            size = scanner.nextInt();
+            array = new int[size];
+
+            System.out.print("Enter Elements of Array = ");
+            for (int i = 0; i < size; i++) {
+                array[i] = scanner.nextInt();
+            }
+        }
+
+        void reverse() {
+            reversedArray = new int[size];
+            int i = 0, j = size - 1;
+
+            while (i < array.length) {
+                reversedArray[i] = array[j];
+                i++;
+                j--;
+            }
+
+            System.out.print("Reversed Array: ");
+            for (int k = 0; k < reversedArray.length; k++) {
+                System.out.print(reversedArray[k] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    class SortArray {
+        private int[] array;
+        private int size;
+
+        void input() {
+            System.out.print("Enter Size of Array = ");
+            size = scanner.nextInt();
+            array = new int[size];
+
+            System.out.print("Enter Elements of Array = ");
+            for (int i = 0; i < size; i++) {
+                array[i] = scanner.nextInt();
+            }
+        }
+
+        void sort() {
+            for (int i = 0; i < array.length; i++) {
+                for (int j = i + 1; j < array.length; j++) {
+                    if (array[i] > array[j]) {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+
+        void output() {
+            System.out.print("Sorted Array: ");
+            for (int i = 0; i < array.length; i++) {
+                System.out.print(array[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    class Search {
+        private int index = -1;
+        private int[] array = {1, 2, 5, 7, 9, 54, 3};
+        private int num;
+
+        void input() {
+            System.out.print("Enter Number You Want to Search = ");
+            num = scanner.nextInt();
+
+            for (int i = 0; i < array.length; i++) {
+                if (num == array[i]) {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (index != -1) {
+                System.out.println("Index of Element is = " + index);
+            } else {
+                System.out.println("Element not found in the array.");
+            }
+        }
+    }
 
     public static void main(String[] args) {
-        Large l = new Large();
-        l.input();
-        l.find();
-        l.Peak();
-        l.maximum();
-        l.minimum();
+        Array array = new Array();
+        Array.Large large = array.new Large();
+
+        large.find();
+        large.peak();
+        large.maximum();
+        large.minimum();
+
+        Array.ReverseArray reverseArray = array.new ReverseArray();
+        reverseArray.input();
+        reverseArray.reverse();
+
+        Array.SortArray sortArray = array.new SortArray();
+        sortArray.input();
+        sortArray.sort();
+        sortArray.output();
+
+        Array.Search search = array.new Search();
+        search.input();
+
     
-       ReverseArray r = new ReverseArray();
-       r.Input();
-       r.reverse();
-
-
-
-
+        array.scanner.close();
+    }
 }
-}
+
      
     
     
