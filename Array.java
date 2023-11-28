635c2a78c7bb1eb78394eeee32a98ef48a93170e
Nofal71 <hafiznofal76@gmail.com>
2023-11-28 22:22:10 +0500

    Kth Largest And Smallest Values (Array Problem)

--- Array.java
+++ Array.java
@@ -113,33 +113,88 @@ public class Array {
     }
 
     class Search {
-        private int index = -1;
-        private int[] array = {1, 2, 5, 7, 9, 54, 3};
+        private int array[];;
         private int num;
-
-        void input() {
-            System.out.print("Enter Number You Want to Search = ");
-            num = scanner.nextInt();
-
-            for (int i = 0; i < array.length; i++) {
-                if (num == array[i]) {
-                    index = i;
-                    break;
-                }
+        private int num2;
+        Scanner kthLarge = new Scanner(System.in);
+        Scanner KthSmall = new Scanner(System.in);
+       
+        public void SortingArray() {
+            for (int i = 0; i < array.length-1; i++) {
+           for(int j = i+1 ; j<array.length;j++){
+            if(array[i]>array[j]){
+                int temp = array[i];
+                array[i]= array[j];
+                array[j]= temp;
             }
-
-            if (index != -1) {
-                System.out.println("Index of Element is = " + index);
-            } else {
-                System.out.println("Element not found in the array.");
-            }
+           }
         }
     }
+    public void PrintSortedArray(){
 
+    
+           for(int k : array){
+            System.out.print( k+ " ");
+     
+        }
+       System.out.println();
+    }
+    public void KthVAlues(){
+        System.out.print("Enter kth Largest value = ");
+        num = kthLarge.nextInt();
+        System.out.print("Enter kth Smallest value = ");
+        num2 = KthSmall.nextInt();
+        if (num<=0||num>array.length||num2>array.length||num2<=0) {
+            return;
+        }else{
+        int largest = array[array.length-num];
+        int smallest = array[num2-1];
+        System.out.println("Kth Largest Value is = "+ largest);
+        System.out.println("Kth Smallest Value is = "+ smallest);
+    }}
+        
+    }
+
+
+    class Polymorphism{
+        // Polymorphism is OOP concept 
+        // different functions with same name
+
+        String name ;
+        int age ;
+    public void printinfo(String name){
+     System.out.println(name);
+     }
+    public void printinfo(int age , String name){
+     System.out.println(name + age);
+    }
+    public void printinfo(int age){
+     System.out.println(age);
+    }
+    }
+
+    class Inheretence{
+        String color;
+        public void area(int height , int length){
+            System.out.println("Display Area = "+ height*length );
+        }
+    }
+    class shape extends Inheretence{
+    public void output(int shape){
+        System.out.println(shape+color);
+    }
+    }
+    class Circle extends shape{
+        int area;
+        public void area(int radius){
+            System.out.println("Radius of circle is = "+ radius+color);
+            
+        }
+    } 
     public static void main(String[] args) {
         Array array = new Array();
+        
         Array.Large large = array.new Large();
-
         large.find();
         large.peak();
         large.maximum();
@@ -154,11 +209,30 @@ public class Array {
         sortArray.sort();
         sortArray.output();
 
-        Array.Search search = array.new Search();
-        search.input();
+        Search search = array.new Search();
+        search.array = new int[]{7,5,8,9,2};
+        search.SortingArray();
+        search.PrintSortedArray();
+        search.KthVAlues();
+        
+        
 
-    
-        array.scanner.close();
+        // Polymorphism Obj1 = array.new Polymorphism();
+        // Obj1.printinfo("Nofal");
+        // Obj1.printinfo(18,"Nofal"); 
+        // Obj1.printinfo(18);
+           
+        // shape Obj = array.new shape();
+        // Obj.color =" red";
+        // Obj.output( 24);
+        // Obj.area(10, 20);
+        
+        // Circle ObJ = array.new Circle();
+        // ObJ.area(12);
+        
+        
+        
+
     }
 }
 
